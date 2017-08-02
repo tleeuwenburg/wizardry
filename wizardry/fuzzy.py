cb2e6ecf8912b2e873c10b88be3e80a06ea851b1
Tennessee Leeuwenburg <t.leeuwenburg@bom.gov.au>
2017-08-02 19:39:36 +1000

    Much more functional and interesting

---
+++ wizardry/fuzzy.py
@@ -0,0 +1,96 @@
+from functools import reduce
+import Levenshtein as lv
+import math
+import operator
+
+ALPHA = 0.01  # To avoid zero vaues in multiply and divide
+
+def learn(books):
+    '''
+    @return an agent that knows about all the books
+    '''
+
+    agent = Agent()
+
+    for title, author, lines in books:
+        agent.learn(title, author, lines)
+
+    return agent
+
+def score_words(word1, word2):
+    '''
+    @return a number between 0 and 1, where 0 = perfect match
+
+    Uses a scaled version of the edit distance and the tanh function.
+    '''
+
+    distance = lv.distance(word1, word2)
+    scaled = distance / 3
+    score = math.tanh(scaled) + ALPHA
+    return score
+
+def best_match(xs, ys):
+    '''
+    Return the two strings which best match between xs and ys
+    '''
+
+    matches = []
+    for x in xs:
+        sorted_ys = sorted((score_words(x, y), y) for y in ys)
+        y = sorted_ys[0][1]
+        matches.append((x, y))
+
+    best_match = matches[0]
+    return best_match
+
+
+
+def match(hasha, hashb):
+
+    xs = [x.strip().lower() for x in hasha.split()]
+    ys = [y.strip().lower() for y in hashb.split()]
+
+    length = min(len(xs), len(ys))
+
+    matches = []
+    for i in range(length):
+        x, y = best_match(xs, ys)
+        xs.remove(x)
+        ys.remove(y)
+        matches.append((x, y))
+
+    scores = [score_words(x, y) for (x, y) in matches]
+    score = reduce(operator.mul, scores)
+
+    return score
+
+
+
+class Agent():
+
+    def __init__(self):
+        self.dict = {}
+        self.authors = []
+        self.titles = []
+
+    def learn(self, title, author, lines):
+
+        terrible_hash = title + " " + author
+        self.dict[terrible_hash] = (title, author, lines)
+
+    def __getitem__(self, key):
+
+        items = list(self.dict.items())
+        best_score = 0
+        best_item = None
+
+        for (key, value) in items:
+            title, author, lines = value
+            terrible_hash = title + " " + author
+            score = match(key, terrible_hash)
+            if score > best_score:
+                best_score = score
+                best_item = value
+
+
+        return best_item
--- wizardry/spells.py
+++ wizardry/spells.py
@@ -5,10 +5,106 @@ from unittest.mock import patch
 
 real_print = builtins.print
 
+
+class PlayingCard():
+
+    def __init__(self, value, suit):
+        self.suit = suit
+        self.value = value
+        self.mode = 'bridge'
+
+        self.bridge_suit_order = ['NT', "S", "H", "D", "C"]
+        self.five_suit_order = ['NT', "H", "D", "C", "S"]
+        self.suit_order = self.bridge_suit_order
+
+    def __repr__(self):
+        return "%s %s" % (self.value, self.suit)
+
+    def set_mode(self, mode):
+
+        if mode == 'bridge':
+            self.suit_order = self.bridge_suit_order
+
+        if mode == "500":
+            self.suit_order = self.five_suit_order
+
+        if mode == "random":
+            random.shuffle(self.suit_order)
+
+    def __gt__(self, other):
+
+        if self.value > other.value:
+            return True
+
+        if self.value == other.value:
+            if (self.suit_order.index(self.suit) <
+                self.suit_order.index(other.suit)):
+                return True
+
+        return False
+
+    def __lt__(self, other):
+        return other > self
+
+    def __eq__(self, other):
+
+        if not self.suit == other.suit:
+            return False
+
+        if not self.value == other.value:
+            return False
+
+        return True
+
+
+class OneIndexList(list):
+
+    def __init__(self, content):
+        self.content = list(content)
+
+    def __getitem__(self, index):
+        index -= 1
+        return self.content[index]
+
+class OneIndexCtx():
+
+    def __init__(self, object):
+        self.object = object
+
+
+    def __enter__(self):
+
+        self.original = self.object.copy()
+        self.object['a'] = OneIndexList(self.object['a'])
+
+    def __exit__(self, *args):
+        self.object['a'] = self.original['a']
+
+
+
 def print_and_fuzz(*args):
     new_args = [a + random.random() for a in args]
     real_print(*new_args)
 
+def print_and_image(*args):
+
+    new_args = [a + random.random() for a in args]
+    real_print(*new_args)
+
+    from PIL import Image
+    im = Image.open("cat_plane.jpg")
+    im.show()
+
 def mystify():
     o = patch('builtins.print', new=print_and_fuzz)
     return o
+
+def bamboozle():
+    o = patch('builtins.print', new=print_and_image)
+    return o
+
+
+def index(a):
+
+    OIC = OneIndexCtx(a)
+    return OIC
